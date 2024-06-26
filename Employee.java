e2a5ba650d5fb5e753a04a488b3a9480325060d9
meloditoprakyilmaz <melodiylmz.1999@gmail.com>
2024-06-26 09:57:15 +0300

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,15 +1,37 @@
 package mext.test.rectangle.iki;
 
-public class Employee {
-    private String name;
-    protected double salary;
-    private int age;
+public class Employee { // "public" bu sınıfın her yerden erişilebilir olduğunu belirtir
+    // çalışanın adını tutan değişken
+    private String name; //"private" bu değişkenin sadece bu sınıfın içinden erişilebilir olduğunu belirtir.
+    // çalışsanın maaşını tutan ondalıklı sayı değişkeni
+    protected double salary; // bu değişkene aynı paket içerisinden veya alt sınıflardan erişilebilir
+    // çalışanın yaşını tutan tam sayı değişkeni
+    private int age; //"private" bu değişkenin sadece bu sınıfın içinden erişilebilir olduğunu belirtir.
 
-    public Employee(String name, double salary, int age){
+    public Employee(String name, double salary, int age){ // bu sınıfın bir örneği oluşturulurken çağırılan metottur.
+        this.name = name; //"name" parametresini sınıfın name değişkenine atar.
+        this.salary = salary; //"salary" parametresini sınıfın salary değişkenine atar.
+        this.age = age; //"age" parametresini sınıfın age değişkenine atar.
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
         this.salary = salary;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
+
+    // Getter ve Setter metotları;
+    // Bu metotlar, özel değişkenlerin değerlerini dış dünyaya (başka sınıflara yani) vermek için kullanılır.
     public String getName(){
         return this.name;
     }
--- Engineer.java
+++ Engineer.java
@@ -1,10 +1,17 @@
 package mext.test.rectangle.iki;
 
-public class Engineer extends Employee {
-    public Engineer(String name, double salary, int age) {
-        super(name, salary, age);
+public class Engineer extends Employee { // public bu sınıfın her yerinden erişilebilir olduğunu belirtir.
+    //'extends' employee engineer classını(sınıfını) employee sınıfından şifre aldığını belirtir.
+    //'employee' sınıfından miras aldığını belirtir.yani engineer sınıfının tüm özelliklerine ve metotlarına sahip olur.
+
+    public Engineer(String name, double salary, int age) { //bu sınıfın yapıcı örneğini oluşturulurken çağırılan metodudur.
+        super(name, salary, age); // 'employee' sınıfının yapıcı metodunu çağırır.'name','salary','age', parametrelerini
+        //'employee'sınıfına iletir.
     }
-    public double getAnnualBonus() {
+    public double getAnnualBonus() { // enginner ın yıllık bonusunu hesaplayan metottur ve bu metotu döndürür.
         return super.salary * .85;
+        // bu metot employee sınıfından alınan maaş/salary değişkenine erişir.
+        //Ne ile erişir? super anahtar kelimesi ile erişir.
+        //Metotun genel işlevi ise maaşın %5 ini hesaplar ve döndürür.Bu engineerın yıllık bonusunu belirler.
     }
 }
